c02599a5f53c547e2c2a365b6cffddd60666e7d7
mbodenchak <mbodenchak@outlook.com>
2022-06-17 12:39:10 -0700

    displaying page with templates

--- index.js
+++ index.js
@@ -2,20 +2,62 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+//Functions
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+//CONSTANTS
+//Templates
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+//JSON DATA
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 //SERVER//
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("Hello from the overview page");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    //Product Page
   } else if (pathName === "/product") {
     res.end("Hello from the product page");
+    //API
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      console.log(productData);
-    });
-    res.end("API");
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    //Not Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
